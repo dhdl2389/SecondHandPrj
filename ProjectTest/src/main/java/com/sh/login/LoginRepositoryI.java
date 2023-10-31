a8c28e87816285a3b8c74a3fd3a376dc95cad219
dhdl2389 <136558510+dhdl2389@users.noreply.github.com>
2023-10-31 09:32:05 +0900

    Update LoginRepositoryI.java

--- ProjectTest/src/main/java/com/sh/login/LoginRepositoryI.java
+++ ProjectTest/src/main/java/com/sh/login/LoginRepositoryI.java
@@ -3,7 +3,7 @@ package com.sh.login;
 import java.util.List;
 
 public interface LoginRepositoryI {
-    public boolean checklogin(LoginDTO loginDTO);
+        public boolean checklogin(LoginDTO loginDTO);
 	LoginDTO selectAll(LoginDTO loginDTO);
 	
 	public int update(LoginDTO loginDTO);
